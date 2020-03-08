49f2767b098016c42db38da45bbf5e2ee6e87a83
Nour Alsafadi <nouralsafadi@hotmail.com>
2020-03-08 00:43:24 -0500

    Update 1-dim-LED.py

--- solution-code/pwm-distance/1-dim-LED.py
+++ solution-code/pwm-distance/1-dim-LED.py
@@ -10,8 +10,7 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(led_R, GPIO.OUT) #initialize pin as output 
 
-pwm_R = GPIO.PWM(led_R, 5000) #initialize PWM pin, 
-frequency = 5kHz
+pwm_R = GPIO.PWM(led_R, 5000) #initialize PWM pin, frequency = 5kHz
 pwm_R.start(0) #start pin at 0% duty cycle 
 
 duty = 0 #modify this statement and look at LED
